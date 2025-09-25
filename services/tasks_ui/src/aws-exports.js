3284d3ccb8f9ac46ee81eb86fc67942511339ee7
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2025-09-25 19:56:20 -0300

    Adição da inteface de tasks do usuário

---
+++ services/tasks_ui/src/aws-exports.js
@@ -0,0 +1,7 @@
+export default {
+  Auth: {
+    region: 'eu-west-1',
+    userPoolId: 'eu-west-1_LxY0bcdh9',
+    userPoolWebClientId: '4luhqua3nul2sp9iu3fgvm1g5g'
+  }
+};
--- services/tasks_ui/src/main.js
+++ services/tasks_ui/src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+
+app.mount('#app');
